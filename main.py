a2fd7ae836ea560fa6db509a814e4500bfd5ed8b
Berke Tuna <btuna14@ku.edu.tr>
2020-04-25 22:44:04 +0200

    README written, code comments added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from stepper_driver import Stepper
 
-set_pins = (11, 13, 16, 18) # IN1 - IN2 - IN3 -IN4
-motor = Stepper(set_pins)
+set_pins = (11, 13, 16, 18) # Four output pins on RPi which are connected to IN1-IN2-IN3-IN4
+motor = Stepper(set_pins) # Instantiated stepper object
+
--- stepper_driver.py
+++ stepper_driver.py
@@ -5,32 +5,30 @@ from time import sleep
 class Stepper():# Stepper object with its driver
     def __init__(self, pins):
         self.pins = pins
-        self.cycle_step = 515
-        self.phases = ((1,0,0,0),
-                       (1,1,0,0),
-                       (0,1,1,0),
-                       (0,0,1,0),
-                       (0,0,1,1),
-                       (0,0,0,1),
-                       (1,0,0,1))
+        self.cycle_step = 515 #Number of steps needed for stepper motor to
+                              # approximately rotate 360 degrees  
+        sleep_time = 0.00085# How fast the steps initiate after one another. Keep as is.
+        self.phases = ((1,0,0,0), #
+                       (1,1,0,0), # 
+                       (0,1,1,0), #
+                       (0,0,1,0), # High pin timing to drive the 4-input stepper driver
+                       (0,0,1,1), #
+                       (0,0,0,1), #
+                       (1,0,0,1)) #
 
     def rotate(self, cycles, direction):
         GPIO.setmode(GPIO.BOARD) # GPIO pin configuration according to their position
-        GPIO.setup(self.pins, GPIO.OUT, initial= GPIO.LOW)
-        sleep_time = 0.00085
-        total_step = (self.cycle_step)*cycles
+        GPIO.setup(self.pins, GPIO.OUT, initial= GPIO.LOW) # Initialize the pins for rotation
+        total_step = (self.cycle_step)*cycles # Total steps needed for input cycle
+        
         for step in range(total_step):
             for phase in range(7):
-                if direction == 'ccw':
+                if direction == 'ccw': # Turn counter-clockwise
                     GPIO.output(self.pins, self.phases[phase])
-                    sleep(sleep_time)
-                if direction == 'cw':
+                    sleep(self.sleep_time)
+                if direction == 'cw': # Turn clockwise
                     GPIO.output(self.pins, self.phases[-phase])
-                    sleep(sleep_time)
+                    sleep(self.sleep_time)
             if step  == (total_step-1):
-                GPIO.cleanup()
-
-    def cleanws(self):
-        GPIO.cleanup()
-
+                GPIO.cleanup() # Cleaning up the used pins after the rotation is finished
 
