492d9fa329b45b297b89c2e1315d084b2be953db
Berke Tuna <btuna14@ku.edu.tr>
2020-04-27 07:39:56 +0100

    dual phase stepping is added

--- stepper_driver.py
+++ stepper_driver.py
@@ -5,15 +5,25 @@ from time import sleep
 class Stepper():# Stepper object with its driver
     def __init__(self, pins):
         self.pins = pins
-        self.cycle_step = 515 #Number of steps needed for stepper motor to
+        self.cycle_step = 512 #Number of steps needed for stepper motor to
                               # approximately rotate 360 degrees  
-        self.sleep_time = 0.002# How fast the steps initiate after one another. Keep as is.
-        self.phases = ((1,1,0,0), #
-                       (1,1,1,0), # 
-                       (0,1,1,0), #
-                       (0,1,1,1), # High pin timing to drive the 4-input stepper driver
-                       (0,0,1,1), #
-                       (1,0,1,1), #
+        self.sleep_time = 0.003# How fast the steps initiate after one another. Keep as is.
+                                  
+      #                           # High pin timings to drive the 4-input stepper driver
+      # self.phase =   ( 
+      #                (1,0,0,0), #
+      #                (1,1,0,0), # 
+      #                (0,1,0,0), #                 --Half Stepping--
+      #                (0,1,1,0), #
+      #                (0,0,1,0), #
+      #                (0,0,1,1), #
+      #                (0,0,0,1), #
+      #                (1,0,0,1)) #
+        
+        self.phase =   ( 
+                       (1,1,0,0), #
+                       (0,1,1,0), #             --Dual Phase Stepping--
+                       (0,0,1,1), #                 
                        (1,0,0,1)) #
 
     def rotate(self, cycles, direction):
@@ -22,12 +32,12 @@ class Stepper():# Stepper object with its driver
         total_step = (self.cycle_step)*cycles # Total steps needed for input cycle
         
         for step in range(total_step):
-            for phase in range(7):
+            for phase in range(4):
                 if direction == 'ccw': # Turn counter-clockwise
-                    GPIO.output(self.pins, self.phases[phase])
+                    GPIO.output(self.pins, self.phase[phase])
                     sleep(self.sleep_time)
                 if direction == 'cw': # Turn clockwise
-                    GPIO.output(self.pins, self.phases[-phase])
+                    GPIO.output(self.pins, self.phase[-phase])
                     sleep(self.sleep_time)
             if step  == (total_step-1):
                 GPIO.cleanup() # Cleaning up the used pins after the rotation is finished
